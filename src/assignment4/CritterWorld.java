21e1ee31af3ae8c51eec880a9beaa658523c691f
thienson-ho <thiensonho@utexas.edu>
2018-03-20 15:56:27 -0500

    Added all files

---
+++ src/assignment4/CritterWorld.java
@@ -0,0 +1,15 @@
+package assignment4;
+
+import java.util.ArrayList;
+
+public class CritterWorld {
+    private int width;
+    private int height;
+    private ArrayList<Critter> world;
+
+    public CritterWorld() {
+        width = Params.world_width;
+        height = Params.world_height;
+
+    }
+}
