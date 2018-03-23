7117b208f0a3861f446d7aecda8837e8598100dd
ArjunSingh1 <arjunsingh@utexas.edu>
2018-03-22 20:50:17 -0500

    Added comments to main and Critter4

--- src/assignment4/Critter4.java
+++ src/assignment4/Critter4.java
@@ -10,9 +10,10 @@
  * Slip days used: <0>
  * Spring 2018
  * 
- * The Critter4 class describes a vicious Critter that multiplies and eats all the Algae it can. It is like a microbe
- * that only feeds on algae, it doesnt  eat any other organism. It will rapidly teraform the map with this and potentially
- * feed many predators, a bloom.  
+ * 
+ * The Critter4 class describes a vicious Critter that multiplies and eats all the Algae it can (an army). It is like a microbe
+ * that only feeds on algae, it doesn't  eat any other organism. It will rapidly teraform the map with this and potentially
+ * feed many predators, a bloom. The species doesn't move, it acts like a weed. If there no immediately adjacent algae it dies out
  *  
  */
 package assignment4;
@@ -21,11 +22,18 @@ import java.util.ArrayList;
 
 public class Critter4 extends Critter{
 	
+	// stores the random direction "seed"
 	int rand;
+	// defines whether a Critter4 can reproduce based on value
 	int proCreate;
+	// determines if the Critter4 can reproduce again 
 	int reload;
+	//The number of algae fought by this Critter4
 	int algaeKill;
 	
+	/**
+	 * Default constructor
+	 */
 	public Critter4() {
 		rand = Critter.getRandomInt(8);
 		proCreate = -5;
@@ -33,6 +41,10 @@ public class Critter4 extends Critter{
 		algaeKill = 0;
 	}
 	
+	/**
+	 * Constructor that redefines the initial limit to proCreating
+	 * @param limit the new proCreate and reload values
+	 */
 	public Critter4(int limit) {
 		rand = Critter.getRandomInt(8);
 		proCreate = limit;
@@ -40,30 +52,42 @@ public class Critter4 extends Critter{
 	}
 
 	
+	/**
+	 * Solves if Critter4 should multiply
+	 */
 	@Override
 	public void doTimeStep() {
+		//if the proCreate is on cool down, decrement the timer
 		if(proCreate >= 3) {
 			reload--;
 		}
+		//if the timer hits 0, reset the timer and allow the organism to start reproducing again 
 		if(reload == 0) {
 			proCreate = 0;
 			reload = 5;
 		}
+		//procreate up to 2 times in a single time step
 		for(int i = 0; i < 2; i++) {
 			if(proCreate < 3) {
 				rand = Critter.getRandomInt(8);
 				proCreate++;
-				Critter4 aCritter = new Critter4(-10);
+				Critter4 aCritter = new Critter4(proCreate);
 				
 				reproduce(aCritter, (i+rand)%8);
 			}
 		}
 	}
+	
+	/**
+	 * @param opponent the toString of the critter to fight
+	 */
 	public boolean fight(String opponent) {
+		//Always fight the algae and increment algaeKill
 		if(opponent.equals("@")) {
 			algaeKill++;
 			return true;
 		}
+		//If the weed is large enough, put up a fight
 		if(this.getEnergy() > 50) {
 			proCreate--;
 			return true;
@@ -71,20 +95,31 @@ public class Critter4 extends Critter{
 		return false;
 	}
 	
+	/**
+	 * returns the string representation of Critter4
+	 */
 	public String toString() {
 		return("4");
 	}
 	
+	/**
+	 * Displays Statistics on the army of weeds via print statements
+	 * @param critter4s
+	 */
 	public static void runStats(java.util.List<Critter> critter4s) {
-
+		//the total number of algae killed by the living members of the army
         int algaeFought = 0;
+        //the size of the army
         int army = 0;
 
+        //Increments the counters
         for (Object obj : critter4s) {
             Critter4 c = (Critter4) obj;
             algaeFought += c.algaeKill;
             army++;
         }
+        
+        //the String output
         if(army > 1) {
         	System.out.println(army + " Critter4s threw hands with " + algaeFought + " @");
         }else if(army < 1) {
--- src/assignment4/Main.java
+++ src/assignment4/Main.java
@@ -73,89 +73,134 @@ public class Main {
         /* Do not alter the code above for your submission. */
         /* Write your code below. */
         
-        boolean errorFlag = false;
+        
+        //a flag that quits program when false
         boolean quitFlag = true;
         
         //remove this
 //        fakeMake();
 
         while(quitFlag) {
+        	//Display prompt
         	System.out.print("critters>");
+        	//read in next line
         	String input = kb.nextLine();
+        	//remove tabs and spaces
         	String inputF = input.replaceAll(" ","");
         	inputF = inputF.replaceAll("	","");
-
+        	
+        	//if the first index is q, analyze input for quit
         	if(input.indexOf("q") != -1 && inputF.indexOf("q") == 0) {
-        		String newInput = input.replaceAll(" ","");
-        		newInput = newInput.replaceAll("	","");
-
+        		String newInput = inputF;
+        		
+        		//If it is a quit, flip the flag
         		if(processQuit(input)) {
         			quitFlag = false;
+        			
+        		//Check if quit is contained to throw error message
         		}else if(newInput.indexOf("quit") == 0 && input.contains("quit")){
         			System.out.println("error processing: " + input);
 				}else{
+					//must be invalid command
         			System.out.println("invalid command: " + input);
         		}
+        		
+        	//If first index is S, check for show, step, seed, and stats
         	}else if(input.indexOf("s") != -1 && inputF.indexOf("s") == 0) {
-        		String newInput = input.replaceAll(" ","");
-            	newInput = newInput.replaceAll("	", "");
+        		String newInput = inputF;
+        		
+        		//check if input is a attempt at show
         		if(newInput.indexOf("show") == 0 && input.indexOf("show") != -1) {
+        			//display world if valid
         			if(processShow(input)) {
 						Critter.displayWorld();
-					}
-					else{
+					
+        			//must be error otherwise
+        			}else{
 						System.out.println("error processing: " + input);
 					}
         		
+        			
+        		//check if input is a attempt at step
         		}else if(newInput.indexOf("step") == 0 && input.indexOf("step") != -1) {
         			boolean isValidSwitch = processStep(input.replace("step", ""));
-
+        			
+        			//if valid and equals step, execute a single step
         			if(newInput.equals("step") && isValidSwitch) {
         				Critter.worldTimeStep();
+        				
+        			//if valid, call multiple time steps
         			}else if(processStep(input.replace("step", ""))) {
+        				//remove extra words
         				newInput = input.replace("step", "");
         				newInput = newInput.replaceAll(" ", "");
         				newInput = newInput.replaceAll("	", "");
+        				
+        				//parse the String to an int in base 10
         				int loop = Integer.parseInt(newInput,10);
+        				
+        				//Call time step (parameter) times
         				for(int i = 0; i < loop; i++) {
         					Critter.worldTimeStep();
         				}
         			}
         			else{
+        				//must be an error
 						System.out.println("error processing: " + input);
 					}
         		
+        			
+        		//check if input is a attempt at seed
         		}else if(newInput.indexOf("seed") == 0 && input.indexOf("seed") != -1) {
+        			//if valid, set seed
         			if(processSeed(input.replace("seed", ""))) {
+        				//remove all extra words
 						newInput = input.replace("seed", "");
 						newInput = newInput.replaceAll(" ", "");
 						newInput = newInput.replaceAll("	", "");
+						
+						//cast and set seed in base 10
 						int seed = Integer.parseInt(newInput, 10);
 						Critter.setSeed(seed);
 					}else{
+						//invalid value passed
 						System.out.println("error processing: " + input);
 					}
         		
+        			
+        		//check if input is a attempt at stats
         		}else if(newInput.indexOf("stats") == 0 && input.indexOf("stats") != -1) {
+        			//call to process invokes stats
         			if(!processStats(input.replace("stats", ""))){
+        				//error handling
 						System.out.println("error processing: " + input);
 					}
+        			
+        		//must be an invalid command
         		}else{
         			System.out.println("invalid command: " + input);
         		}
         		
+        		
+        	//If first index is m, analyze input for make
         	}else if(input.indexOf("m") != -1 && inputF.indexOf("m") == 0) {
-        		String newInput = input.replaceAll(" ","");
-            	newInput = newInput.replaceAll("	", "");
+        		String newInput = inputF;
+        		
+        		//Check to see if its an attempt at make
         		if(newInput.indexOf("make") == 0 && input.indexOf("make") != -1) {
         			newInput = input.replace("make","");
+        			
+        			//Make called in process, handle error if process fails
         			if(!processMake(newInput)) {
         				System.out.println("error processing: " + input);
         			}
         		}
         		else {
+        			//must be invalid
         			System.out.println("invalid command: " + input);
         		}
+        		
+        	//must be an invalid command	
         	}else {
         		System.out.println("invalid command: " + input);
         	}
@@ -166,28 +211,55 @@ public class Main {
 
     }
     
+    
+    /**
+     * Determines if input is a valid quit command
+     * @param input the string input
+     * @return true if valid, false otherwise
+     */
     public static boolean processQuit(String input) {
+    	//removes tabs and spaces
     	String newInput = input.replaceAll(" ","");
     	newInput = newInput.replaceAll("	", "");
+    	
+    	// if the element is exactly quit then input is valid
     	if(newInput.equals("quit") && input.indexOf("quit") != -1) {
     		return true;
     	}
     	return false;
     }
     
+    
+    /**
+     * Determines if input is a valid show command
+     * @param input the string input
+     * @return true if valid, false otherwise
+     */
     public static boolean processShow(String input) {
+    	//if the input is exactly show, the input is correct
     	if(input.equals("show")) {
     		return true;
     	}
     	return false;
     }
     
+    
+    /**
+     * Determines if input is a valid step command
+     * @param input the string input
+     * @return true if valid, false otherwise
+     */
     public static boolean processStep(String input) {
+    	//Remove tabs and spaces
     	String ni = input.replaceAll(" ", "");
     	ni = ni.replaceAll("	", "");
+    	
+    	//if the new String is empty then it must be a ningle Step
     	if(ni.equals("")) {
     		return true;
     	}
+    	
+    	//splits elements by spaces
     	String[] twoInputs = input.split(" ");
     	int length = 0;
     	ArrayList<String> goodElements = new ArrayList<String>();
@@ -198,9 +270,13 @@ public class Main {
     			goodElements.add(x);
     		}
     	}
+    	
+    	//if there is exactly one good element attempt a cast
     	if(length == 1) {
     		try{
+    			//convert element to int
     			int loop = Integer.parseInt(goodElements.get(0),10);
+    			//only positive steps allowed
     			if(loop <= 0) {
     				return false;
     			}
@@ -212,9 +288,18 @@ public class Main {
     	return false;
     }
     
+    
+    /**
+     * Determines if input is a valid seed command
+     * @param input the string input
+     * @return true if valid, false otherwise
+     */
     public static boolean processSeed(String input) {
+    	//splits elements by spaces
     	String[] twoInputs = input.split(" ");
     	int length = 0;
+    	
+    	//Counts a valid element if String isn't empty or only contains a space
     	ArrayList<String> goodElements = new ArrayList<String>();
     	for(String x : twoInputs) {
     		x = x.replaceAll("	", "");
@@ -223,10 +308,13 @@ public class Main {
     			goodElements.add(x);
     		}
     	}
+    	
+    	//If there is one good element, attempt a single seed command
     	if(length == 1) {
     		try{
-    			int loop = Integer.parseInt(goodElements.get(0),10);
-    			if(loop < 0) {
+    			//convert element to int
+    			int seed = Integer.parseInt(goodElements.get(0),10);
+    			if(seed < 0) {
     				return false;
     			}
     			return true;
@@ -237,10 +325,19 @@ public class Main {
     	return false;
     }
     
+    
+    /**
+     * Determines if input is a valid stats command
+     * @param input the string input
+     * @return true if valid, false otherwise
+     */
     public static boolean processStats(String input) {
+    	//splits elements by spaces
     	String[] twoInputs = input.split(" ");
     	int length = 0;
     	ArrayList<String> goodElements = new ArrayList<String>();
+    	
+    	//Counts a valid element if String isn't empty or only contains a space
     	for(String x : twoInputs) {
     		x = x.replaceAll("	", "");
     		if(!(x.equals(""))) {
@@ -248,12 +345,16 @@ public class Main {
     			goodElements.add(x);
     		}
     	}
+    	
+    	//If there is one good element, attempt a single stats command
     	if(length == 1) {
     		try{
-
+    			//make package name from element
     			List<Critter> critterList = Critter.getInstances(goodElements.get(0));
     			Class critterType = Class.forName(myPackage + "." + goodElements.get(0));
     			Method m = critterType.getMethod("runStats", java.util.List.class);
+    			
+    			//invoke method
 				m.invoke(critterList.getClass(), critterList);
 				return true;
     		}catch(Exception e) {
@@ -263,10 +364,19 @@ public class Main {
     	return false;
     }
     
+    
+    /**
+     * Determines if input is a valid make command
+     * @param input the string input
+     * @return true if valid, false otherwise
+     */
     public static boolean processMake(String input) {
+    	//splits elements by spaces
     	String[] twoInputs = input.split(" ");
     	int length = 0;
     	ArrayList<String> goodElements = new ArrayList<String>();
+    	
+    	//Counts a valid element if String isn't empty or only contains a space
     	for(String x : twoInputs) {
     		x = x.replaceAll("	", "");
     		if(!(x.equals(""))) {
@@ -274,19 +384,26 @@ public class Main {
     			goodElements.add(x);
     		}
     	}
+    	
+    	// If there is one good element, attempt a single make statement
     	if(length == 1) {
+    		//attempt to pass the element as the class name
     		try {
     			Critter.makeCritter(goodElements.get(0));
     			return true;
     		}catch(Exception e) {
     			return false;
     		}
+    		
+    	//if there are two good elements, attempt a looped make statement
     	}else if(length == 2) {
     		try{
+    			//convert second parameter to an int in base 10
     			int loop = Integer.parseInt(goodElements.get(1),10);
     			if(loop <= 0) {
     				return false;
     			}
+    			//loop make statement 
     			for(int i = 0; i < loop; i++) {
     				Critter.makeCritter(goodElements.get(0));
     			}
@@ -298,15 +415,22 @@ public class Main {
     	return false;
     }
     
+    
+    /**
+     * Creates 100 Algae and 25 Craigs
+     */
     public static void fakeMake() {
     	try {
+    		//loop for 100 Algae
     		for(int i = 0; i < 100; i++) {
     			Critter.makeCritter("Algae");
     		}
+    		//loop for 25 Algae
     		for(int i = 0; i < 25; i++) {
     			Critter.makeCritter("Craig");
     		}
     	}catch(Exception e){
+    		//Catch exceptions in making Critters
     		System.out.println("fakeMe() didn't work");
     	}
     }
